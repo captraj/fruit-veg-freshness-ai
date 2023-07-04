d4c2a7080a5c6bec9b5f77164ef58f535d0349d4
Biswaraj Choudhury <76588598+captraj@users.noreply.github.com>
2023-07-04 20:20:14 +0530

    converted to code to utilize functions

--- evaluate-image.py
+++ evaluate-image.py
@@ -3,10 +3,16 @@ import numpy as np
 from keras.models import load_model
 
 
-def evaluate_rotten_vs_fresh(image_path):
-    # Load the trained model
-    model = load_model('trained-freshness-model.h5')
+# Classify fresh/rotten
+def print_fresh_rotten(res):
+    threshold_fresh = 0.2                # set according to standards
+    if res < threshold_fresh:            # implement two thresholds later, for med,rotten and fresh ident
+        print("The item is FRESH!")         
+    else:
+        print("The item is ROTTEN")
 
+
+def pre_proc_img(image_path):
     # Read the image using OpenCV
     img = cv2.imread(image_path)
     img = cv2.resize(img, (100, 100))
@@ -15,9 +21,15 @@ def evaluate_rotten_vs_fresh(image_path):
     # Preprocess the image
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
+    return img
 
-    # Make the prediction
-    prediction = model.predict(img)
+
+def evaluate_rotten_vs_fresh(image_path):
+    # Load the trained model
+    model = load_model('trained-freshness-model.h5')
+
+    # Read and process and predict
+    prediction = model.predict(pre_proc_img(image_path))
 
     return prediction[0][0]
 
@@ -25,12 +37,4 @@ def evaluate_rotten_vs_fresh(image_path):
 # Example usage:
 img_path = 'image-to-eval.png'
 is_rotten = evaluate_rotten_vs_fresh(img_path)
-print(f'Prediction: {is_rotten}')
-
-# Classify fresh/rotten
-
-threshold_fresh = 0.5  # set according to standards
-if is_rotten < threshold_fresh:
-    print("The item is FRESH!")
-else:
-    print("The item is ROTTEN")
+print(f'Prediction: {is_rotten}',print_fresh_rotten(is_rotten))
